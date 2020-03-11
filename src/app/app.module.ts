432440be9043ff502228fb3b93a6c15b2fe14203
Etec 244 <e244ti@outlook.com>
2020-03-11 12:22:28 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { CidadesModule } from './cidades/cidades.module';
 import { RouterModule, Routes } from '@angular/router';
 import { CidadesPesquisaComponent } from './cidades/cidades-pesquisa/cidades-pesquisa.component';
 import { CidadeService } from './cidades/cidades.service';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
   {path: 'cidades', component: CidadesPesquisaComponent }
@@ -21,6 +22,7 @@ const routes: Routes = [
     BrowserModule,
     RouterModule.forRoot(routes),
     AppRoutingModule,
+    HttpClientModule,
     CoreModule,
     CidadesModule
 
